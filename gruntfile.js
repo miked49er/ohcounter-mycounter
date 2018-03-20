35590a9fc05100602556c227648ec31583919da3
Mike Deiters <miked49er@gmail.com>
2018-03-19 20:58:56 -0400

    Added http-server

--- gruntfile.js
+++ gruntfile.js
@@ -34,6 +34,20 @@ module.exports = function(grunt) {
         } // options
       } // dev
     }, // compass
+    'http-server': {
+      'dev': {
+        root: './',
+        port: 3000,
+        host: '0.0.0.0',
+        cache: 0,
+        showDir: false,
+        autoIndex: true,
+        ext: 'html',
+        runInBackground: true,
+        https: false,
+        openBrowser: false
+      }
+    }, // http-server
     watch: {
       options: { livereload: true },
       scripts: {
@@ -55,8 +69,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks('grunt-http-server');
 
   grunt.registerTask('js', ['uglify']);
   grunt.registerTask('css', ['compass:dev']);
+  grunt.registerTask('server', ['http-server:dev']);
+  grunt.registerTask('dev', ['http-server:dev', 'watch']);
   grunt.registerTask('default', ['watch']);
 };
